6511ac7ce8fd3d874348381e98f41809eeecb5b9
Nancy Tran <nhvt@uw.edu>
2017-10-19 15:28:13 -0700

    Add end of class code on 10/19/2017

--- tasksvr/main.go
+++ tasksvr/main.go
@@ -5,6 +5,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/nhvtran/info344-in-class/tasksvr/handlers"
+	"github.com/nhvtran/info344-in-class/tasksvr/models/tasks"
+
+	mgo "gopkg.in/mgo.v2"
 )
 
 const defaultAddr = ":80"
@@ -19,9 +24,20 @@ func main() {
 	//construct the appropriate tasks.Store
 	//construct the handlers.Context
 
+	// note: because I'm using Windows Home, use 192.168.99.100 instead of localhost because of the Linux VIM
+	// another note: mongoDB is at 192.168.99.100:27017;
+	// in reality, you're supposed to use localhost for running Go API server
+	mongoSess, err := mgo.Dial("192.168.99.100")
+	if err != nil {
+		log.Fatalf("error dialing mongo: %v", err)
+	}
+	mongoStore := tasks.NewMongoStore(mongoSess, "tasks", "tasks")
+
+	handlerCtx := handlers.NewHandlerContext(mongoStore)
+
 	mux := http.NewServeMux()
-	//mux.HandleFunc("/v1/tasks", TODO: add TasksHandler )
-	//mux.HandleFunc("/v1/tasks/", TODO: add SpecificTaskHandler )
+	mux.HandleFunc("/v1/tasks", handlerCtx.TasksHandler)
+	mux.HandleFunc("/v1/tasks/", handlerCtx.SpecificTaskHandler)
 
 	fmt.Printf("server is listening at http://%s...\n", addr)
 	log.Fatal(http.ListenAndServe(addr, mux))
