7006a2a49772520e9ddf6aeda6a435f717c571b6
Nancy Tran <nhvt@uw.edu>
2017-10-19 13:33:59 -0700

    Add leftover class code from 10/17/2017

--- hmac/main_test.go
+++ hmac/main_test.go
@@ -7,6 +7,24 @@ func TestSign(t *testing.T) {
 	//use strings.NewReader() to get an io.Reader
 	//interface over a simple string
 	//https://golang.org/pkg/strings/#NewReader
+
+	// echo "a value to sign" | hmac sign somesecretkey
+	// >> returns the digital signature in the terminal
+	cases := []struct {
+		input          string
+		signingKey     string
+		expectedOutput string
+	}{
+		{
+			input:          "hello",
+			signingKey:     "somesecretkey",
+			expectedOutput: "8nnOkxiw7ywvBiTTpAisbXIfEat9ru3HNBrnQmB7aMU=",
+		},
+	}
+
+	for _, c := range cases {
+
+	}
 }
 
 func TestVerify(t *testing.T) {
